f8d8a8d6e2c96913aca203b8dad532d9b232b530
Tim Huang <timhuang304@gmail.com>
2024-07-11 06:00:56 +0800

    add neural line test
    Signed-off-by: Tim Huang <timhuang304@gmail.com>

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{Rng, RngCore};
 
 #[derive(Debug)]
 pub struct Network {
@@ -10,7 +10,7 @@ impl Network {
 		Self { layers }
 	}
 
-	pub fn random(neurons_per_layer: Vec<usize>) -> Self {
+	pub fn random(rng: &mut dyn RngCore, neurons_per_layer: Vec<usize>) -> Self {
 		assert!(neurons_per_layer.len() > 1);
 		let mut built_layers = Vec::new();
 
@@ -18,7 +18,7 @@ impl Network {
 			let input_size = neurons_per_layer[i];
 			let output_size = neurons_per_layer[i + 1];
 
-			built_layers.push(Layer::random(input_size, output_size));
+			built_layers.push(Layer::random(rng, input_size, output_size));
 		}
 
 		Self {layers: built_layers}
@@ -52,10 +52,10 @@ impl Layer {
 		outputs
 	}
 
-	fn random(input_size: usize, output_size: usize) -> Self {
+	fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
 		let mut neurons = Vec::new();
 		for _ in 0..output_size {
-			neurons.push(Neuron::random(input_size));
+			neurons.push(Neuron::random(rng, input_size));
 		}
 		Self {neurons}
 	}
@@ -80,8 +80,8 @@ impl Neuron {
 		(self.bias + output).max(0.0)
 	}
 
-	fn random(input_size: usize) -> Self {
-		let mut rng = rand::thread_rng();
+	fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
+		// let mut rng = rand::thread_rng();
 		let bias = rng.gen_range(-1.0..=1.0);
 
 		let weights = (0..input_size)
@@ -91,3 +91,29 @@ impl Neuron {
 		Self {bias, weights}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use approx::assert_relative_eq;
+	use rand::SeedableRng;
+	use rand_chacha::ChaCha8Rng;
+
+	#[test]
+	fn random() {
+		let mut rng = ChaCha8Rng::from_seed(Default::default());
+		let neuron = Neuron::random(&mut rng, 4);
+		assert_relative_eq!(neuron.bias, -0.6255188);
+		assert_relative_eq!(neuron.weights.as_slice(), [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref());
+	}
+
+	#[test]
+	fn propagate() {
+		let neuron = Neuron {
+			bias: 0.5,
+			weights: vec![-0.3, 0.8],
+		};
+
+		assert_relative_eq!(neuron.propagate(&[-10.0, -10.0]), 0.0);
+		assert_relative_eq!(neuron.propagate(&[0.5, 1.0]), (0.5 * -0.3 + 1.0 * 0.8 + 0.5));
+	} }
