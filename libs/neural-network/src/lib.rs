e1f6ced9116b6ca9edf7dbfcd106ec72e41820c9
Tim Huang <timhuang304@gmail.com>
2024-07-11 01:23:36 +0800

    complete neural network part
    Signed-off-by: Tim Huang <timhuang304@gmail.com>

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,9 +1,30 @@
+use rand::Rng;
+
 #[derive(Debug)]
 pub struct Network {
 	layers: Vec<Layer>,
 }
 
 impl Network {
+	pub fn new(layers: Vec<Layer>) -> Self {
+		Self { layers }
+	}
+
+	pub fn random(neurons_per_layer: Vec<usize>) -> Self {
+		assert!(neurons_per_layer.len() > 1);
+		let mut built_layers = Vec::new();
+
+		for i in 0..neurons_per_layer.len() - 1 {
+			let input_size = neurons_per_layer[i];
+			let output_size = neurons_per_layer[i + 1];
+
+			built_layers.push(Layer::random(input_size, output_size));
+		}
+
+		Self {layers: built_layers}
+
+	}
+
 	pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
 		for layer in &self.layers {
 			inputs = layer.propagate(inputs);
@@ -17,7 +38,7 @@ impl Network {
 }
 
 #[derive(Debug)]
-struct Layer {
+pub struct Layer {
 	neurons: Vec<Neuron>
 }
 
@@ -30,6 +51,15 @@ impl Layer {
 		}
 		outputs
 	}
+
+	fn random(input_size: usize, output_size: usize) -> Self {
+		let mut neurons = Vec::new();
+		for _ in 0..output_size {
+			neurons.push(Neuron::random(input_size));
+		}
+		Self {neurons}
+	}
+
 }
 
 #[derive(Debug)]
@@ -47,7 +77,17 @@ impl Neuron {
 			output += inputs[i] * self.weights[i];
 		}
 		
-		output.max(0.0)
+		(self.bias + output).max(0.0)
+	}
 
+	fn random(input_size: usize) -> Self {
+		let mut rng = rand::thread_rng();
+		let bias = rng.gen_range(-1.0..=1.0);
+
+		let weights = (0..input_size)
+			.map(|_| rng.gen_range(-1.0..=1.0))
+			.collect();
+
+		Self {bias, weights}
 	}
 }
